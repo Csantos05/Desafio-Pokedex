218895684f5b64269b2144df562cf1408ae3a77c
Cleison Santos <cleisonsantosrj79@gmail.com>
2024-07-30 21:38:43 -0300

    desafio pokedex

---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,19 @@
+class Pokemon {
+    id;
+    name;
+    photo;
+
+    type;
+    types = [];  
+
+    ability;
+    abilities = [];
+   
+    baseStat;
+    stats = [];
+   
+    nameStats;
+    stat = []; 
+}
+
+
